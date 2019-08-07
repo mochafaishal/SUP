c9c72dca82b72e9efb1b7abbe41a05277cae88b5
unknown <mochammadfaishal21@gmail.com>
2019-08-07 23:09:08 +0700

    get user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
   public function index()
-  {
-      return view('user');
+  {   
+      $users = DB::table('users')->get();
+    	return view('user',['users' => $users]);
   }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -20,43 +20,17 @@
                                                 <th>update at</th>                                
                                             </tr>
                                         </thead>
-                                        <tbody>
+                                        <tbody>  
+                                            @foreach($users as $users)
                                             <tr>
-                                                <td>2018-09-29 05:57</td>
-                                                <td>Mobile</td>
-                                                <td>iPhone X 64Gb Grey</td>
-                                                <td class="process">Processed</td>
-                                            </tr>
+                                                <td>{{ $users->id }}</td>
+                                                <td>{{ $users->name }}</td>
+                                                <td>{{ $users->email }}</td>
+                                                <td>{{ $users->created_at }}</td>
+                                                <td>{{ $users->updated_at }}</td>
                                             <tr>
-                                                <td>2018-09-28 01:22</td>
-                                                <td>Mobile</td>
-                                                <td>Samsung S8 Black</td>
-                                                <td class="process">Processed</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-27 02:12</td>
-                                                <td>Game</td>
-                                                <td>Game Console Controller</td>
-                                                <td class="denied">Denied</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-26 23:06</td>
-                                                <td>Mobile</td>
-                                                <td>iPhone X 256Gb Black</td>
-                                                <td class="denied">Denied</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-25 19:03</td>
-                                                <td>Accessories</td>
-                                                <td>USB 3.0 Cable</td>
-                                                <td class="process">Processed</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-29 05:57</td>
-                                                <td>Accesories</td>
-                                                <td>Smartwatch 4.0 LTE Wifi</td>
-                                                <td class="denied">Denied</td>
-                                            </tr>
+                                            @endforeach
+                                      
                                         </tbody>
                                     </table>
                                 </div>
